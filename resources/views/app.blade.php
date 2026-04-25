6c6f494746b49305b56f0501ad93ade75c2d280b
r5q0 <bilalemailsss@gmail.com>
2026-04-25 06:30:19 +0200

    added working favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="theme-color" content="#000000">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Tally</title>
+    <title>bunq Tally</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     @viteReactRefresh
 @vite(['resources/css/app.css', 'resources/js/main.tsx'])
 </head>
